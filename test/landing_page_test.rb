b6106d60283a21f265179b80eaefbe734554a0fe
Mike Gehard <mike.gehard@gmail.com>
2013-12-20 13:27:36 -0700

    Add test for header and some color so students can see where it ends.

--- test/landing_page_test.rb
+++ test/landing_page_test.rb
@@ -8,4 +8,11 @@ describe "Landing page" do
 
     page.must_have_css 'html head title', visible: false
   end
+
+  it "displays the restaurant name in the header of the page" do
+    visit "/"
+
+    header = page.find('html body header')
+    header.text.wont_equal ''
+  end
 end
