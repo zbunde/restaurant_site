467da80f5d9f24dc4e8f3f51f708a9c887891c3c
Mike Gehard <mike.gehard@gmail.com>
2013-12-20 13:19:36 -0700

    Add test for title.

--- test/landing_page_test.rb
+++ test/landing_page_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 describe "Landing page" do
   include Capybara::DSL
 
-  it "have the proper body text" do
+  it "displays the restaurant name in the browser window" do
     visit "/"
 
-    page.must_have_content "Hello World!"
+    page.must_have_css 'html head title', visible: false
   end
 end
