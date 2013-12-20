feaeb5c45cc85c0f07c8826d5f375d490c6f6278
Mike Gehard <mike.gehard@gmail.com>
2013-12-20 14:07:27 -0700

    Test the text in all of the elements.

--- test/landing_page_test.rb
+++ test/landing_page_test.rb
@@ -21,6 +21,10 @@ describe "Landing page" do
 
     welcome_text = page.all('html body p')
     welcome_text.length.must_equal 2
+
+    welcome_text.each do |element|
+      element.text.wont_equal ''
+    end
   end
 
   it "displays a list of 5 dishes" do
@@ -28,6 +32,10 @@ describe "Landing page" do
 
     definition_terms = page.all('html body dl dt')
     definition_terms.length.must_equal 5
+
+    definition_terms.each do |element|
+      element.text.wont_equal ''
+    end
   end
 
   it "displays a description for each of 5 dishes" do
@@ -35,5 +43,9 @@ describe "Landing page" do
 
     definition_descriptions = page.all('html body dl dd')
     definition_descriptions.length.must_equal 5
+
+    definition_descriptions.each do |element|
+      element.text.wont_equal ''
+    end
   end
 end
