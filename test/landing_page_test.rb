2d9e9644e32ca78e6793523a97bc4844080356c0
Mike Gehard <mike.gehard@gmail.com>
2014-01-02 10:09:55 -0700

    Mark all specs as pending

--- test/landing_page_test.rb
+++ test/landing_page_test.rb
@@ -8,15 +8,18 @@ describe "Landing page" do
   end
 
   it "displays the restaurant name in the browser window" do
+    skip
     page.must_have_css 'html head title', visible: false
   end
 
   it "displays the restaurant name in the header of the page" do
+    skip
     header = page.find('html body header')
     header.text.wont_equal ''
   end
 
   it "displays two paragraphs of welcome text" do
+    skip
     welcome_text = page.all('html body main p')
     welcome_text.length.must_equal 2
 
@@ -26,6 +29,7 @@ describe "Landing page" do
   end
 
   it "displays a list of 5 dishes" do
+    skip
     definition_terms = page.all('html body main dl dt')
     definition_terms.length.must_equal 5
 
@@ -35,6 +39,7 @@ describe "Landing page" do
   end
 
   it "displays a description for each of 5 dishes" do
+    skip
     definition_descriptions = page.all('html body main dl dd')
     definition_descriptions.length.must_equal 5
 
@@ -44,28 +49,33 @@ describe "Landing page" do
   end
 
   it "displays the restaurant address in the footer" do
+    skip
     address = page.find('html body footer address')
     address.text.wont_equal ''
   end
 
   it "displays a contact us email link in the footer" do
+    skip
     mailto_link = page.find('html body footer a[href^=mailto]')
     mailto_link.text.wont_equal ''
   end
 
   it "displays the copyright in the footer" do
+    skip
     footer = page.find('html body footer')
     footer.text.must_match /©/
     footer.text.must_match /2013/
   end
 
   it "does not display the copyright in the address" do
+    skip
     address = page.find('html body footer address')
     address.text.wont_match /©/
     address.text.wont_match /2013/
   end
 
   it "displays an image of the restaurant" do
+    skip
     image = page.find('html body main img')
     image[:src].wont_be_nil
     image[:src].must_match /images\//
