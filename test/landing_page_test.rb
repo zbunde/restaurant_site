9124ed787fb1e617524ac0dcdef27114ae5e29b3
Zach Klabunde <zbunde@gmail.com>
2014-03-05 16:49:47 -0700

    added address, email link, picture, copyright date

--- test/landing_page_test.rb
+++ test/landing_page_test.rb
@@ -13,13 +13,11 @@ describe "Landing page" do
   end
 
   it "displays the restaurant name in the header of the page" do
-    skip
     header = page.find('html body header')
     header.text.wont_equal ''
   end
 
   it "displays two paragraphs of welcome text" do
-    skip
     welcome_text = page.all('html body main p')
     welcome_text.length.must_equal 2
 
@@ -29,7 +27,6 @@ describe "Landing page" do
   end
 
   it "displays a list of names for 5 dishes" do
-    skip
     dishes = page.all('html body main ul li')
     dishes.length.must_equal 5
 
@@ -39,26 +36,22 @@ describe "Landing page" do
   end
 
   it "displays the restaurant address in the footer" do
-    skip
     address = page.find('html body footer address')
     address.text.wont_equal ''
   end
 
   it "displays a contact us email link in the footer" do
-    skip
     mailto_link = page.find('html body footer a[href^=mailto]')
     mailto_link.text.wont_equal ''
   end
 
   it "displays the copyright in the footer" do
-    skip
     footer = page.find('html body footer')
     footer.text.must_match /©/
     footer.text.must_include "2013"
   end
 
   it "displays an image of the restaurant" do
-    skip
     image = page.find('html body main img')
     image[:src].wont_be_nil
     image[:src].must_match /images\//
